1b2408641ff5130ed648d99649fe6c24079f2f2d
srinathgadde <srinathgaddde@gmail.com>
2024-01-21 17:38:01 +0530

    basic counter-application developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [counter, setCounter] = useState(0);
+  // let counter = 10;
 
+  const addValue = () => {
+    // console.log("Clicked", Math.random());
+    if (counter < 20) {
+      counter = counter + 1;
+      setCounter(counter);
+    }
+  };
+
+  const removeValue = () => {
+    // counter = counter - 1;
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        <i>Counter-App</i>
+      </h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+
+      <br />
+      <button
+        onClick={() => {
+          setCounter(0);
+        }}
+      >
+        Reset Value
+      </button>
+
+      <footer>footer: {counter}</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
